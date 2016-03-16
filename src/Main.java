5c2f459956fb86a830b4410b185d738485590d6a
hoddi84 <hoddipsx@hotmail.co.uk>
2016-03-16 22:55:56 +0000

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,45 @@
+/**
+ * Created by Hoddi on 16.3.2016.
+ */
+
+import org.json.JSONObject;
+
+import java.net.URL;
+import java.util.Scanner;
+import java.io.IOException;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        /*
+         * limit specifies the length of our ConceptNet5 query.
+         */
+        String limit = "10";
+        String queryWhat = "animal";
+        int limitToInt = Integer.valueOf(limit);
+
+        String s = "http://conceptnet5.media.mit.edu/data/5.3/c/en/" + queryWhat + "?limit=" + limit;
+        URL url = new URL(s);
+
+        /*
+         * Read the URL
+         */
+        Scanner scan = new Scanner(url.openStream());
+        String str = new String();
+        while (scan.hasNext())
+            str += scan.nextLine();
+        scan.close();
+
+        /*
+         * Create the JSON object using org.json library
+         */
+        JSONObject obj = new JSONObject(str);
+
+        for (int i = 0; i < limitToInt; i++) {
+            JSONObject res2 = obj.getJSONArray("edges").getJSONObject(i);
+            System.out.println(res2.getString("start").replaceAll("/c/en/", ""));
+        }
+    }
+}
