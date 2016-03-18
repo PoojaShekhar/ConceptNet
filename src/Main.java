fbabf6838fec728c47aca4dc6dcee08f49798bf1
hoddi84 <hoddipsx@hotmail.co.uk>
2016-03-18 00:47:25 +0000

    Added ConceptQuery

--- src/Main.java
+++ src/Main.java
@@ -13,19 +13,21 @@ public class Main {
 
         ConceptQuery conceptQuery = new ConceptQuery();
 
-        String animals = conceptQuery.returnURL("animal",5);
+        String animals = conceptQuery.returnURL("animal",10);
         JSONObject obj = new JSONObject(animals);
 
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 10; i++) {
             JSONObject results = obj.getJSONArray("edges").getJSONObject(i);
-            String currAnimal = results.getString("start").replaceAll("/c/en/", ""); // added
-            System.out.println(results.getString("start").replaceAll("/c/en/", ""));
+            String currAnimal = results.getString("start").replaceAll("/c/en/", "");
+            //System.out.println(results.getString("start").replaceAll("/c/en/", ""));
 
             String animalInfo = conceptQuery.returnURL(currAnimal, 5);
             JSONObject obj2 = new JSONObject(animalInfo);
+
+            System.out.println("--------------------" + currAnimal + "---------------------------");
             for (int k = 0; k < 5; k++) {
                 JSONObject results2 = obj2.getJSONArray("edges").getJSONObject(k);
-                System.out.println(results2.getString("surfaceText").replaceAll("[,]", ""));
+                System.out.println(results2.getString("surfaceText").replaceAll("\\[", "").replaceAll("\\]", ""));
             }
         }
     }
