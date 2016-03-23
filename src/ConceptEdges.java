dffeb8d7c67c95698e9692550077498bb746bf45
hoddi84 <hoddipsx@hotmail.co.uk>
2016-03-23 15:00:41 +0000

    Minor changes

---
+++ src/ConceptEdges.java
@@ -0,0 +1,30 @@
+/**
+ * Created by Hoddi on 23.3.2016.
+ */
+public class ConceptEdges {
+
+    public static final String atLocationEdge = "AtLocation";
+    public static final String relatedToEdge = "RelatedTo";
+    public static final String hasAEdge = "HasA";
+    public static final String isAEdge = "IsA";
+    public static final String capableOfEdge = "CapableOf";
+    public static final String desiresEdge = "Desires";
+    public static final String usedForEdge = "UsedFor";
+    public static final String partOfEdge = "PartOf";
+    public static final String hasPropertyEdge = "HasProperty";
+    public static final String memberOfEdge = "MemberOf";
+    public static final String causesEdge = "Causes";
+    public static final String hasSubeventEdge = "HasSubevent";
+    public static final String hasFirstSubeventEdge = "HasFirstSubevent";
+    public static final String hasLastSubeventEdge = "HasLastSubevent";
+    public static final String hasPrerequisiteEdge = "HasPrerequisite";
+    public static final String motivatedByGoalEdge = "MotivatedByGoal";
+    public static final String obstructedByEdge = "ObstructedBy";
+    public static final String cretedByEdge = "CreatedBy";
+    public static final String synonymEdge = "Synonym";
+    public static final String antonymEdge = "Antonym";
+    public static final String derivedFromEdge = "DerivedFrom";
+    public static final String translationOfEdge = "TranslationOf";
+    public static final String definedAsEdge = "DefinedAs";
+
+}
--- src/Main.java
+++ src/Main.java
@@ -25,7 +25,9 @@ public class Main {
 
     public static void main(String[] args) throws IOException, JSONException {
 
-
+        boolean conceptRelations = true;
+        boolean conceptSimilarity = false;
+        boolean conceptMatches = false;
         int animalQuery = 20;
         int sheepQuery = 30;
         int goatQuery = 30;
@@ -40,6 +42,10 @@ public class Main {
         LinkedList<String> SameList = new LinkedList<>();
         LinkedList<String> conceptList = new LinkedList<>();
         TreeMap<String, Integer> comparedList = new TreeMap<>();
+
+        ///////////////////////////////////////////////////////////////////////
+        /////////////////////////// Simple ConceptNet5 Query //////////////////
+        ///////////////////////////////////////////////////////////////////////
         /*
          * Query ConceptNet for "animal".
          */
@@ -57,91 +63,105 @@ public class Main {
                 AnimalsUnique.add(Animals.get(i));
             }
         }
+        ////////////////////////////////////////////////////////////////////
+        /////////////// Counting matches between concepts //////////////////
+        ////////////////////////////////////////////////////////////////////
+        if (conceptMatches) {
         /*
          * Query concept1.
          */
-        String sheepQ = ConceptQuery.returnURL(concept1, sheepQuery);
-        JSONObject jsonSheep = new JSONObject(sheepQ);
+            String sheepQ = ConceptQuery.returnURL(concept1, sheepQuery);
+            JSONObject jsonSheep = new JSONObject(sheepQ);
 
-        for (int i = 0; i < sheepQuery; i++) {
-            JSONObject resSheep = jsonSheep.getJSONArray("edges").getJSONObject(i);
-            String firstSheep = resSheep.getString("end").replaceAll("/c/en/", "");
-            Sheep.add(firstSheep);
-        }
-        Sheep = makeUnique(Sheep);
+            for (int i = 0; i < sheepQuery; i++) {
+                JSONObject resSheep = jsonSheep.getJSONArray("edges").getJSONObject(i);
+                String firstSheep = resSheep.getString("end").replaceAll("/c/en/", "");
+                Sheep.add(firstSheep);
+            }
+            Sheep = makeUnique(Sheep);
         /*
          * Query concept2.
          */
-        String goatQ = ConceptQuery.returnURL(concept2, goatQuery);
-        JSONObject jsonGoat = new JSONObject(goatQ);
+            String goatQ = ConceptQuery.returnURL(concept2, goatQuery);
+            JSONObject jsonGoat = new JSONObject(goatQ);
 
-        for (int i = 0; i < goatQuery; i++) {
-            JSONObject resGoat = jsonGoat.getJSONArray("edges").getJSONObject(i);
-            String firstGoat = resGoat.getString("end").replaceAll("/c/en/","");
-            Goat.add(firstGoat);
-        }
-        Goat = makeUnique(Goat);
+            for (int i = 0; i < goatQuery; i++) {
+                JSONObject resGoat = jsonGoat.getJSONArray("edges").getJSONObject(i);
+                String firstGoat = resGoat.getString("end").replaceAll("/c/en/", "");
+                Goat.add(firstGoat);
+            }
+            Goat = makeUnique(Goat);
 
-        System.out.println("------------" + concept1.toUpperCase() + "----------------");
-        for (int i = 0; i < Sheep.size(); i++) {
-            System.out.println(Sheep.get(i));
-        }
-        System.out.println();
+            System.out.println("------------" + concept1.toUpperCase() + "----------------");
+            for (int i = 0; i < Sheep.size(); i++) {
+                System.out.println(Sheep.get(i));
+            }
+            System.out.println();
 
-        System.out.println("-------------" + concept2.toUpperCase() + "---------------");
-        for (int i = 0; i < Goat.size(); i++) {
-            System.out.println(Goat.get(i));
-        }
-        System.out.println();
+            System.out.println("-------------" + concept2.toUpperCase() + "---------------");
+            for (int i = 0; i < Goat.size(); i++) {
+                System.out.println(Goat.get(i));
+            }
+            System.out.println();
         /*
          * Similarities between concept1 & concept2.
          */
-        for (int i = 0; i < Goat.size(); i++) {
-            for (int k = 0; k < Sheep.size(); k++) {
-                if (Goat.get(i).equals(Sheep.get(k))) {
+            for (int i = 0; i < Goat.size(); i++) {
+                for (int k = 0; k < Sheep.size(); k++) {
+                    if (Goat.get(i).equals(Sheep.get(k))) {
                         SameList.add(Goat.get(i));
+                    }
                 }
             }
-        }
         /*
          * Counting similarities between a concept and a list of other concepts.
          */
-        System.out.println("----------SIMILARITIES: " + concept1.toUpperCase() + " & " + concept2.toUpperCase() + "-------------");
-        for (int i = 0; i < SameList.size(); i++) {
-            System.out.println(SameList.get(i));
-        }
-        System.out.println();
-
-        String conceptItem = "dog";
-        System.out.println("---------LIST OF MOST SIMILAR FOR: " + conceptItem.toUpperCase() + "------------------");
-        conceptList.add("mouse");
-        conceptList.add("bird");
-        conceptList.add("cat");
-        conceptList.add("puma");
-        conceptList.add("spoon");
-        conceptList.add("cougar"); // fyrir Tryggva.
-        conceptList.add("car");
-        conceptList.add("chicken");
-        conceptList.add("squirrel");
-        comparedList = ConceptSimilarity.returnSimilar(conceptItem, conceptList);
-        System.out.println(comparedList);
-        System.out.println();
-
-
-        String conceptChildren = "finch";
-        System.out.println("-----------FIND CHILDREN OF: " + conceptChildren.toUpperCase() + "---------");
-        LinkedList<String> listChildren = ConceptRelations.findChildren(conceptChildren);
-        for (int i = 0; i < listChildren.size(); i++) {
-            System.out.println(listChildren.get(i));
-        }
-        System.out.println();
-
-        String conceptParent = "goldfinch";
-        System.out.println("-----------FIND PARENT OF: " + conceptParent.toUpperCase() + "---------");
-        LinkedList<String> listParent = ConceptRelations.findParents(conceptParent);
-        for (int i = 0; i < listParent.size(); i++) {
-            System.out.println(listParent.get(i));
+            System.out.println("----------SIMILARITIES: " + concept1.toUpperCase() + " & " + concept2.toUpperCase() + "-------------");
+            for (int i = 0; i < SameList.size(); i++) {
+                System.out.println(SameList.get(i));
+            }
+            System.out.println();
         }
 
+        ///////////////////////////////////////////////////////////////////////
+        ///////////////////// ConceptSimilarity ///////////////////////////////
+        ///////////////////////////////////////////////////////////////////////
+        if (conceptSimilarity) {
+            String conceptItem = "dog";
+            System.out.println("---------LIST OF MOST SIMILAR FOR: " + conceptItem.toUpperCase() + "------------------");
+            conceptList.add("mouse");
+            conceptList.add("bird");
+            conceptList.add("cat");
+            conceptList.add("puma");
+            conceptList.add("spoon");
+            conceptList.add("cougar"); // fyrir Tryggva.
+            conceptList.add("car");
+            conceptList.add("chicken");
+            conceptList.add("squirrel");
+            comparedList = ConceptSimilarity.returnSimilar(conceptItem, conceptList);
+            System.out.println(comparedList);
+            System.out.println();
+        }
+
+        ////////////////////////////////////////////////////////////////////////
+        /////////////////////// ConceptRelations ///////////////////////////////
+        ////////////////////////////////////////////////////////////////////////
+        if (conceptRelations) {
+            String conceptChildren = "machine";
+            System.out.println("-----------FIND CHILDREN OF: " + conceptChildren.toUpperCase() + "---------");
+            LinkedList<String> listChildren = ConceptRelations.findChildren(conceptChildren);
+            for (int i = 0; i < listChildren.size(); i++) {
+                System.out.println(listChildren.get(i));
+            }
+            System.out.println();
+
+            String conceptParent = "mammal";
+            System.out.println("-----------FIND PARENT OF: " + conceptParent.toUpperCase() + "---------");
+            LinkedList<String> listParent = ConceptRelations.findParents(conceptParent);
+            for (int i = 0; i < listParent.size(); i++) {
+                System.out.println(listParent.get(i));
+            }
+            System.out.println();
+        }
     }
 }
