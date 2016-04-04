0679220b5b645440a5aaeb66553af03daf3c2373
hoddi84 <hoddipsx@hotmail.co.uk>
2016-04-04 02:17:40 +0000

    Minor changes

--- src/Main.java
+++ src/Main.java
@@ -31,24 +31,28 @@ public class Main {
 
         while (scanner.hasNext()) {
             String parent = scanner.next();
-            String child = scanner.next();
             String relation = scanner.next();
+            String child = scanner.next();
             Double weight = Double.parseDouble(scanner.next());
             Node newNode = new Node(parent, child, relation, weight);
             nodeColl.add(newNode);
         }
 
-        for (int i = 0; i < nodeColl.size(); i++) {
-            for (int k = 0; k < nodeColl.size(); k++) {
-                if (((ArrayList<Node>)nodeColl).get(i).child.equals(((ArrayList<Node>)nodeColl).get(k).parent)) {
-                    nodeRel.add(((ArrayList<Node>)nodeColl).get(i));
-                    nodeRel.add(((ArrayList<Node>)nodeColl).get(k));
-                }
+        for (int k = 0; k < nodeColl.size(); k++) {
+            if (((ArrayList<Node>)nodeColl).get(0).child.equals(((ArrayList<Node>)nodeColl).get(k).parent)) {
+                nodeRel.add(((ArrayList<Node>)nodeColl).get(0));
+                nodeRel.add(((ArrayList<Node>)nodeColl).get(k));
             }
         }
 
+        System.out.println(((ArrayList<Node>)nodeColl).get(0).child);
+        System.out.println(((ArrayList<Node>)nodeColl).get(3).parent);
         for (int i = 0; i < nodeRel.size(); i++) {
             System.out.println(((ArrayList<Node>)nodeRel).get(i));
+        }
+
+        for (int i = 0; i < nodeColl.size(); i++) {
+            System.out.println(((ArrayList<Node>)nodeColl).get(i));
         }
     }
 
